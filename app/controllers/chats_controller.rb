09c843a3f1865d6e31dc5272a52d963d3743533e
Marina Diaz <m.diaz@mailistec.fr>
2026-03-06 11:53:28 +0100

    chats refinement in progress

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -10,7 +10,10 @@ class ChatsController < ApplicationController
   end
 
   def index
-    @chat = Chat.all(user: current_user)
-    @message = Message.all(user: current_user)
+    @message = Message.find(params[:massage_id])
+    @chat = current_user.chats.where(message: @message) && !@chat.empty?
+    @chat = Chat.new
+    @chats = Chat.all(user: current_user.chat)
+    @messages = Message.all(user: current_user)
   end
 end
